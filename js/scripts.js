325358f883c9abace05ed65761d02fa7b9e5b26c
Dustin Starke <dustin.starke@gmail.com>
2018-08-17 15:32:23 -0700

    change the behavior of my output to replace individual numbers with desired output instead of entire array

--- js/scripts.js
+++ js/scripts.js
@@ -2,40 +2,33 @@
 var numbersWithZero = "Beep!";
 var numbersWithOne = "Boop!";
 var numbersDivisThree = "I'm sorry Dave. I'm afraid I can't do that.";
-console.log(numbersWithZero, numbersWithOne, numbersDivisThree);
 
-var numberTransform = function(userInput) {
+
+var numberInsert = function(userInput) {
   var rangeShown = [];
   if (userInput < 0) {
     alert("Please enter a positive number.");
   }
-
   for (var i = 0; i <= userInput; i++) {
-    if (userInput % 3 === 0) {
-      var subNumber = numbersDivisThree;
-    }
-    // console.log(userInput);
-    else if (userInput.toString().includes("1")) {
-      var subNumber = numbersWithOne;
-    } else if (userInput.toString().includes("0")) {
-      var subNumber = numbersWithZero;
+    if (i > 0 && i % 3 === 0) {
+      rangeShown.push(numbersDivisThree);
+    } else if (i.toString().includes("1")) {
+      rangeShown.push(numbersWithOne);
+    } else if (i.toString().includes("0")) {
+      rangeShown.push(numbersWithZero);
     } else {
       rangeShown.push(i);
-      var subNumber = (rangeShown.toString()).split(",").join(", ");
     }
   }
-  return subNumber;
-
+  return rangeShown.toString().split(",").join(", ");
 };
-
-
 // user-interface logic
 $(document).ready(function() {
   $("#inputForm").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("#userInput").val());
     var result = "";
-    result += numberTransform(userInput);
+    result += numberInsert(userInput);
     $("#finalOutput").text(result);
     $("#results").show();
   });
