f61b3097dcbc16554b64a5d72bb46c92daff020d
Dustin Starke <dustin.starke@gmail.com>
2018-08-17 09:14:00 -0700

    address first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
+// business logic
+
+function numberTransform(userInput) {}
+var numbersWithZero =
+var numbersWithOne =
+var numbersDivisThree =
+
 // user-interface logic
 $(document).ready(function(){
   $("form#inputForm").submit(function(event){
     event.preventDefault();
-
-    var numbersWithZero =
-    var numbersWithOne =
-    var numbersDivisbyThree =
-    
+    var userInput = parseInt($("userInput").val());
+    var result = "";
+    result += numberTransform(userInput);
+    $("finalOutput").text(result);
+    $("results").show();
   });
 });
