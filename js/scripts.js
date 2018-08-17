985abe6cfb9dbc7e306b653193f8c00ffae503ac
Dustin Starke <dustin.starke@gmail.com>
2018-08-17 12:27:15 -0700

    fix a bug that prevented array from displaying

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,32 @@
 // business logic
 var numbersWithZero = "Beep!";
 var numbersWithOne = "Boop!";
-var numbersDivisThree = "I'm sorry Dave. I'm afraid I can't do that."
+var numbersDivisThree = "I'm sorry Dave. I'm afraid I can't do that.";
 console.log(numbersWithZero, numbersWithOne, numbersDivisThree);
 
 var numberTransform = function(userInput) {
   var rangeShown = [];
-  var userInputNowString = userInput.toString();
-  // console.log(rangeShown);
-  console.log(userInputNowString);
-
   if (userInput < 0) {
-    return alert("Please enter a positive number.");
+    alert("Please enter a positive number.");
   }
 
   for (var i = 0; i <= userInput; i++) {
     if (userInput % 3 === 0) {
       var subNumber = numbersDivisThree;
-    } else if (userInput.includes(1)) {
+    }
+    // console.log(userInput);
+    else if (userInput.toString().includes("1")) {
       var subNumber = numbersWithOne;
-    } else if (userInput.includes(0)) {
+    } else if (userInput.toString().includes("0")) {
       var subNumber = numbersWithZero;
     } else {
       rangeShown.push(i);
       var subNumber = (rangeShown.toString()).split(",").join(", ");
     }
+  }
+  return subNumber;
 
-      return subNumber;
-    }
-  };
+};
 
 
 // user-interface logic
@@ -37,7 +35,7 @@ $(document).ready(function() {
     event.preventDefault();
     var userInput = parseInt($("#userInput").val());
     var result = "";
-    result = numberTransform(userInput);
+    result += numberTransform(userInput);
     $("#finalOutput").text(result);
     $("#results").show();
   });
